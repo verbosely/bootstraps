7650769ebb39eb9a1db343b4f55fe80cb58fb882
Zachary Flohr <zmflohr@gmail.com>
2025-05-30 08:23:09 -0400

    Modify print_invalid_versions
    - The caller of print_invalid_versions will pass all HTTP error codes
      that occurred to it; print_invalid_versions will not need to search
      for patterns to determine which message to print to stderr.

--- shared/term_output.sh
+++ shared/term_output.sh
@@ -240,18 +240,7 @@ print_program_lifecycle() {
 }
 
 print_invalid_versions() {
-    local msg
-    case "${1}" in
-        '4xx')
-            shift
-            msg="The following versions returned 4xx status codes and will not "
-            msg+="be installed: $(params_to_csv_string $@)"
-        ;;
-        '5xx')
-            shift
-            msg="The following versions returned 5xx status codes and will not "
-            msg+="be installed: $(params_to_csv_string $@)"
-        ;;
-    esac
+    local msg="The following versions returned HTTP ${1} responses "
+    msg+="and will not be installed: ${2}"
     print_message 1 "yellow" "${msg}"
 }
