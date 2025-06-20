22e3c6b22404487e6f06a5a2e296d17ac4eee92e
Zachary Flohr <zmflohr@gmail.com>
2025-06-20 05:26:06 -0400

    Establish naming convention for private functions

--- shared/checks.bash
+++ shared/checks.bash
@@ -21,7 +21,7 @@ check_binaries() {
     [[ ${!#} = u ]] && unset -f check_binaries
 }
 
-check_conflicting_params() {
+_check_conflicting_params() {
     local illegal
     [ -n "${PURGE}" -a -n "${PAX}" ] && {
         illegal="-p|--purge-all, -P[version]|--purge-all-except[=version]"
@@ -39,7 +39,7 @@ check_conflicting_params() {
         terminate "${illegal}" ; }
 }
 
-check_param_args() {
+_check_param_args() {
     case "$1" in
         '-i'|'--install'|'-P'|'--purge-all-except'|'-r'|'--replace-all-with')
             [[ "$2" =~ ^(([[:digit:]]+\.)*[[:digit:]]+)?$ ]] || terminate "$2"
@@ -56,19 +56,19 @@ check_param_args() {
     esac
 }
 
-define_valid_option_params() {
+_define_valid_option_params() {
     temp=$(getopt --options 'hi::pP::r::' \
         --longoptions 'help,install::,purge-all,purge-all-except::' \
         --longoptions 'replace-all-with::' \
         --name $(basename "${0}") -- "${@:1:$#-1}")
     getopt_exit_status=$?
-    unset -f define_valid_option_params
+    unset -f _define_valid_option_params
 }
 
 check_params() {
     local temp ; local -r USAGE="${!#}" ; local -i getopt_exit_status
     local -a keep_versions install_versions
-    check_binaries "getopt" ; define_valid_option_params $*
+    check_binaries "getopt" ; _define_valid_option_params $*
     (( ${getopt_exit_status} )) && terminate ${getopt_exit_status}
     eval set -- "${temp}" ; unset temp getopt_exit_status
     while true; do
@@ -85,17 +85,17 @@ check_params() {
                 [ -z "${REPLACE}" ] && declare -gr REPLACE="yes";;&
             '-i'|'--install'|'-P'|'--purge-all-except'|\
                     '-r'|'--replace-all-with')
-                check_param_args "$1" "$2"
+                _check_param_args "$1" "$2"
                 shift 2;;
             '--')
                 shift ; break;;
-        esac ; check_conflicting_params
+        esac ; _check_conflicting_params
     done ; (( $# )) && eval ${USAGE} >&2 && exit 1
     [ -z "${INSTALL}${PURGE}${PAX}${REPLACE}" ] && declare -gr REPLACE="yes" &&
-        check_param_args "-r"
+        _check_param_args "-r"
     declare -ag keep_versions=($(sort_and_filter "${keep_versions[@]}"))
     declare -ag install_versions=($(sort_and_filter "${install_versions[@]}"))
-    unset -f check_conflicting_params check_param_args check_params
+    unset -f _check_conflicting_params _check_param_args check_params
 }
 
 check_duplicate_versions() {
